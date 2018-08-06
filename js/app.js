cc94407fd81fdb35d50b25ccc179e70f499939e0
kris riedman <riedmank@gmail.com>
2018-08-06 12:32:19 -0700

    added objects to js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,31 @@
 'use strict';
+
+var firstAndPike = {
+  minCustomers: 23,
+  maxCustomers: 65,
+  avgCustomers: 6.3
+};
+
+var seaTacAirport = {
+  minCustomers: 3,
+  maxCustomers: 24,
+  avgCustomers: 1.2
+};
+
+var seattleCenter = {
+  minCustomers: 11,
+  maxCustomers: 38,
+  avgCustomers: 3.7
+};
+
+var capitolHill = {
+  minCustomers: 20,
+  maxCustomers: 38,
+  avgCustomers: 2.3
+};
+
+var alki = {
+  minCustomers: 2,
+  maxCustomers: 16,
+  avgCustomers: 4.6
+};
