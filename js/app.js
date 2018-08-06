114f2f59b32ed878f658a0cced6e223ce5fb70f1
kris riedman <riedmank@gmail.com>
2018-08-06 16:33:54 -0700

    finished js functionality and sales page

--- js/app.js
+++ js/app.js
@@ -1,47 +1,57 @@
 'use strict';
 
-var hours = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ',
-  '5pm: ', '6pm: ', '7pm: ', '8pm: ', 'Total:'];
-
 var firstAndPike = {
+  name: 'firstAndPike',
   minCustomers: 23,
   maxCustomers: 65,
   avgSales: 6.3,
   customersPerHour: randomCustomer,
-  soldCookies: cookieSales
+  soldCookies: cookieSales,
+  addInfo: addCookieInfo
 };
 
 var seaTacAirport = {
+  name: 'seaTacAirport',
   minCustomers: 3,
   maxCustomers: 24,
   avgSales: 1.2,
   customersPerHour: randomCustomer,
-  soldCookies: cookieSales
+  soldCookies: cookieSales,
+  addInfo: addCookieInfo
 };
 
 var seattleCenter = {
+  name: 'seattleCenter',
   minCustomers: 11,
   maxCustomers: 38,
   avgSales: 3.7,
   customersPerHour: randomCustomer,
-  soldCookies: cookieSales
+  soldCookies: cookieSales,
+  addInfo: addCookieInfo
 };
 
 var capitolHill = {
+  name: 'capitolHill',
   minCustomers: 20,
   maxCustomers: 38,
   avgSales: 2.3,
   customersPerHour: randomCustomer,
-  soldCookies: cookieSales
+  soldCookies: cookieSales,
+  addInfo: addCookieInfo
 };
 
 var alki = {
+  name: 'alki',
   minCustomers: 2,
   maxCustomers: 16,
   avgSales: 4.6,
   customersPerHour: randomCustomer,
-  soldCookies: cookieSales
+  soldCookies: cookieSales,
+  addInfo: addCookieInfo
 };
+
+var hours = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ',
+  '5pm: ', '6pm: ', '7pm: ', '8pm: ', 'Total: '];
 
 function randomCustomer() {
   this.minCustomers = Math.ceil(this.minCustomers);
@@ -59,5 +69,24 @@ function cookieSales() {
     x = x + cookies[i];
   }
   cookies.push(x);
+  for(i = 0; i < cookies.length; i++) {
+    cookies[i] = hours[i] + cookies[i];
+  }
   return cookies;
+}
+
+function addCookieInfo() {
+  var ul = document.getElementById(this.name);
+  for(var j = 0; j < 16; j++) {
+    var storeInfo = document.createElement('li');
+
+    storeInfo.textContent = this.soldCookies()[j];
+    ul.appendChild(storeInfo);
+  }
+}
+
+var storeObjects = [firstAndPike, seaTacAirport, seattleCenter, capitolHill, alki];
+
+for(var i = 0; i < storeObjects.length; i++) {
+  storeObjects[i].addInfo();
 }
