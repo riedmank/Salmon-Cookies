4a722634fd5e8224417896c5bf8688d75ffc0fcc
kris riedman <riedmank@gmail.com>
2018-08-07 13:18:55 -0700

    added constructors and prototypes

--- js/app.js
+++ js/app.js
@@ -1,91 +1,60 @@
 'use strict';
 
-var hours = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ',
-  '5pm: ', '6pm: ', '7pm: ', '8pm: ', 'Total: '];
+// table header
+var header = ['Store Name', '6 am', '7 am', '8 am', '9 am', '10 am', '11 am', '12 pm', '1 pm', '2 pm', '3 pm',
+  '4 pm', '5 pm', '6 pm', '7 pm', '8 pm', 'Total'];
 
-function randomCustomer() {
-  this.minCustomers = Math.ceil(this.minCustomers);
-  this.maxCustomers = Math.floor(this.maxCustomers);
-  return Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1)) + this.minCustomers;
+// Store constructor
+function Store(name, minCustomers, maxCustomers, avgSales, hourlySales) {
+  this.name = name;
+  this.minCustomers = minCustomers;
+  this.maxCustomers = maxCustomers;
+  this.avgSales = avgSales;
+  this.hourlySales = hourlySales;
 }
 
+// Creating objects
+var firstAndPike = new Store('First and Pike', 23, 65, 6.3, this.cookieSales);
+var seaTacAirport = new Store('SeaTac Airport', 3, 24, 1.2, this.cookieSales);
+var seattleCenter = new Store('Seattle Center', 11, 38, 3.7, this.cookieSales);
+var capitolHill = new Store('Capitol Hill', 20, 38, 2.3, this.cookieSales);
+var alki = new Store('alki', 2, 16, 4.6, this.cookieSales);
+
+// Adding prototypes
+Store.prototype.randomSales = randomSales;
+Store.prototype.hourlySales = cookieSales;
+Store.prototype.render = render;
+
+// Calculate random customers and cookie sales
+function randomSales() {
+  this.minCustomers = Math.ceil(this.minCustomers);
+  this.maxCustomers = Math.floor(this.maxCustomers);
+  return (Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1)) + this.minCustomers) * this.avgSales;
+}
+
+// Add store name, cookie sales, and totals to array
 function cookieSales() {
   var cookies = [];
-  for(var i = 0; i < 15; i++) {
-    cookies[i] = Math.round(this.customersPerHour() * this.avgSales);
+  for(var i = 1; i < 15; i++) {
+    cookies[i] = Math.round(this.randomSales());
   }
-  var sum = cookies[0];
-  for(i = 0; i < cookies.length; i++) {
-    sum = sum + cookies[i];
-  }
-  cookies.push(sum);
-  for(i = 0; i < cookies.length; i++) {
-    cookies[i] = `${hours[i]} ${cookies[i]} cookies.`;
-  }
+  cookies.push(cookies.reduce((total, element) => total + element, 0));
+  cookies.splice(0, 1, this.name);
   return cookies;
 }
 
-function addCookieInfo() {
+// add info to page
+function render() {
   var ul = document.getElementById(this.name);
   for(var j = 0; j < 16; j++) {
     var storeInfo = document.createElement('li');
-    storeInfo.textContent = this.soldCookies()[j];
+    storeInfo.textContent = this.hourlySales()[j];
     ul.appendChild(storeInfo);
   }
 }
 
-var firstAndPike = {
-  name: 'firstAndPike',
-  minCustomers: 23,
-  maxCustomers: 65,
-  avgSales: 6.3,
-  customersPerHour: randomCustomer,
-  soldCookies: cookieSales,
-  addInfo: addCookieInfo
-};
-
-var seaTacAirport = {
-  name: 'seaTacAirport',
-  minCustomers: 3,
-  maxCustomers: 24,
-  avgSales: 1.2,
-  customersPerHour: randomCustomer,
-  soldCookies: cookieSales,
-  addInfo: addCookieInfo
-};
-
-var seattleCenter = {
-  name: 'seattleCenter',
-  minCustomers: 11,
-  maxCustomers: 38,
-  avgSales: 3.7,
-  customersPerHour: randomCustomer,
-  soldCookies: cookieSales,
-  addInfo: addCookieInfo
-};
-
-var capitolHill = {
-  name: 'capitolHill',
-  minCustomers: 20,
-  maxCustomers: 38,
-  avgSales: 2.3,
-  customersPerHour: randomCustomer,
-  soldCookies: cookieSales,
-  addInfo: addCookieInfo
-};
-
-var alki = {
-  name: 'alki',
-  minCustomers: 2,
-  maxCustomers: 16,
-  avgSales: 4.6,
-  customersPerHour: randomCustomer,
-  soldCookies: cookieSales,
-  addInfo: addCookieInfo
-};
-
 var storeObjects = [firstAndPike, seaTacAirport, seattleCenter, capitolHill, alki];
 
 for(var i = 0; i < storeObjects.length; i++) {
-  storeObjects[i].addInfo();
+  storeObjects[i].render();
 }
